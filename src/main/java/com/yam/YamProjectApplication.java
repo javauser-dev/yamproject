e8db591520a3be04a1aee9ac5075c767cab4c39c
Seungjin <javacoding9900@gmail.com>
2025-02-21 17:39:05 +0900

    properties에 기본설정만 한 커밋

---
+++ src/main/java/com/yam/YamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yam/YamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
