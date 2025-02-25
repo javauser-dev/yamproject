a22c8565347037fd9384d605bda62965e719bc5d
Seungjin <javacoding9900@gmail.com>
2025-02-25 16:08:34 +0900

    메인페이지 삭제, 등록일 칼럼 추가

--- src/main/java/com/yam/customer/member/domain/Member.java
+++ src/main/java/com/yam/customer/member/domain/Member.java
@@ -1,11 +1,15 @@
 package com.yam.customer.member.domain;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -19,6 +23,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "customer_manage") // 테이블 이름을 customer_manage로 변경
+@EntityListeners(AuditingEntityListener.class) //추가. JPA auditing 활성화
 public class Member {
 
     @Id
@@ -48,4 +53,8 @@ public class Member {
     
     @Column
     private String customerProfileImage;
+    
+    @CreatedDate //자동으로 날짜가 주입.
+    @Column(updatable = false) // 업데이트(수정) 불가
+    private LocalDateTime customerRegDate; // 가입일 필드 추가
 }
