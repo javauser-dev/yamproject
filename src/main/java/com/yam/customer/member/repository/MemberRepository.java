3b74ea8554269a67908fe64c5ac779c35ee8e9d9
Seungjin <javacoding9900@gmail.com>
2025-02-25 14:42:47 +0900

    Revert "오류수정"
    This reverts commit 7d4bc24522b711e2090070ec7b32c6f29bb5a16a.

--- src/main/java/com/yam/customer/member/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yam.customer.member.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.yam.customer.member.domain.Member;
-
-public interface MemberRepository extends JpaRepository<Member, String> { // <Entity, ID 타입>
-    boolean existsByCustomerId(String customerId); // 아이디 중복 확인
-}
